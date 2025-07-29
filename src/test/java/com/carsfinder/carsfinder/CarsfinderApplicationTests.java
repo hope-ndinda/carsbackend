cc6d7f7d84cb40a6ef8a0b56540b4350476027f6
hope-ndinda <hopenishimwe5@gmail.com>
2025-07-29 12:06:47 +0200

    Update application.properties with latest config

--- src/test/java/com/carsfinder/carsfinder/CarsfinderApplicationTests.java
+++ src/test/java/com/carsfinder/carsfinder/CarsfinderApplicationTests.java
@@ -1,15 +1,12 @@
-package com.carsfinder;
+package com.carsfinder.carsfinder;
 
-import com.carsfinder.carsfinder.CarsfinderApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = CarsfinderApplication.class)
-
+@SpringBootTest
 class CarsfinderApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
