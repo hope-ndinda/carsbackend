26a0c80df0f9791ad876d194a4e37dd788b5a4f4
hope-ndinda <hopenishimwe5@gmail.com>
2025-07-30 08:01:43 +0200

    Update for frontend

--- src/main/java/com/carsfinder/carsfinder/service/CloudinaryService.java
+++ src/main/java/com/carsfinder/carsfinder/service/CloudinaryService.java
@@ -1,9 +1,9 @@
 package com.carsfinder.carsfinder.service;
-
+import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import com.cloudinary.Cloudinary;
 
 import java.io.IOException;
 import java.util.Map;
@@ -12,11 +12,15 @@ import java.util.Map;
 public class CloudinaryService {
     private final Cloudinary cloudinary;
 
-    public CloudinaryService() {
+    public CloudinaryService(
+            @Value("${cloudinary.cloud_name}") String cloudName,
+            @Value("${cloudinary.api_key}") String apiKey,
+            @Value("${cloudinary.api_secret}") String apiSecret
+    ) {
         this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "drfnqzkaa",
-                "api_key", "275321128649523",
-                "api_secret", "MgSJGyLy1R2wfpBxkSbvsflSejE"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 
@@ -25,4 +29,3 @@ public class CloudinaryService {
         return uploadResult.get("secure_url").toString();
     }
 }
-
